543ee09205cfc707427ea0072f0e49704e83c870
pulkitj017 <pulkit.jain8004@thepsi.com>
2024-09-18 12:17:28 +0530

    Update merge-details.ps1

--- scripts/merge-details.ps1
+++ scripts/merge-details.ps1
@@ -1,10 +1,76 @@
+# # Define file paths
+# $latestDependenciesFile = "latest-dependencies.txt"
+# $licensesParsedFile = "licenses_parsed.txt"
+# $outputFile = "sbom-result.txt"
+
+# # Read latest dependencies
+# $latestDependencies = @{}
+# Get-Content $latestDependenciesFile | ForEach-Object {
+#     if ($_ -match '^(.*?)\s+(\d+\.\d+\.\d+)\s*$') {
+#         $dependency = $matches[1].Trim()
+#         $latestVersion = $matches[2].Trim()
+#         $latestDependencies[$dependency] = $latestVersion
+#     }
+# }
+
+# # Read licenses parsed
+# $licensesParsed = @{}
+# Get-Content $licensesParsedFile | ForEach-Object {
+#     if ($_ -match '^(.*?)\s+(\d+\.\d+\.\d+)\s+(.+)$') {
+#         $dependency = $matches[1].Trim()
+#         $version = $matches[2].Trim()
+#         $license = $matches[3].Trim()
+#         $licensesParsed[$dependency] = @{ Version = $version; License = $license }
+#     }
+# }
+
+# # Prepare output
+# $output = @()
+# foreach ($dep in $licensesParsed.Keys) {
+#     $licenseInfo = $licensesParsed[$dep]
+#     $output += [PSCustomObject]@{
+#         Dependency = $dep
+#         CurrentVersion = $licenseInfo.Version
+#         LatestVersion = $licenseInfo.Version
+#         License = $licenseInfo.License
+#     }
+# }
+
+# foreach ($dep in $latestDependencies.Keys) {
+#     if ($licensesParsed.ContainsKey($dep)) {
+#         $licenseInfo = $licensesParsed[$dep]
+#         $output += [PSCustomObject]@{
+#             Dependency = $dep
+#             CurrentVersion = $licenseInfo.Version
+#             LatestVersion = $latestDependencies[$dep]
+#             License = $licenseInfo.License
+#         }
+#     } else {
+#         $output += [PSCustomObject]@{
+#             Dependency = $dep
+#             CurrentVersion = "N/A"
+#             LatestVersion = $latestDependencies[$dep]
+#             License = "N/A"
+#         }
+#     }
+# }
+
+# # Exclude rows with all "N/A" values
+# $output = $output | Where-Object {
+#     $_.CurrentVersion -ne "N/A" -or
+#     $_.LatestVersion -ne "N/A" -or
+#     $_.License -ne "N/A"
+# }
+
+# # Write output to file
+# $output | Format-Table -AutoSize | Out-File $outputFile -Encoding UTF8
 # Define file paths
 $latestDependenciesFile = "latest-dependencies.txt"
 $licensesParsedFile = "licenses_parsed.txt"
 $outputFile = "sbom-result.txt"
 
 # Read latest dependencies
-$latestDependencies = @{}
+$latestDependencies = @{ }
 Get-Content $latestDependenciesFile | ForEach-Object {
     if ($_ -match '^(.*?)\s+(\d+\.\d+\.\d+)\s*$') {
         $dependency = $matches[1].Trim()
@@ -14,7 +80,7 @@ Get-Content $latestDependenciesFile | ForEach-Object {
 }
 
 # Read licenses parsed
-$licensesParsed = @{}
+$licensesParsed = @{ }
 Get-Content $licensesParsedFile | ForEach-Object {
     if ($_ -match '^(.*?)\s+(\d+\.\d+\.\d+)\s+(.+)$') {
         $dependency = $matches[1].Trim()
@@ -28,24 +94,25 @@ Get-Content $licensesParsedFile | ForEach-Object {
 $output = @()
 foreach ($dep in $licensesParsed.Keys) {
     $licenseInfo = $licensesParsed[$dep]
+    $latestVersion = if ($latestDependencies.ContainsKey($dep)) { $latestDependencies[$dep] } else { $licenseInfo.Version }
+
+    # Check if versions are different and mark them with **
+    $currentVersion = $licenseInfo.Version
+    if ($currentVersion -ne $latestVersion) {
+        $currentVersion += "**"
+        $latestVersion += "**"
+    }
+
     $output += [PSCustomObject]@{
         Dependency = $dep
-        CurrentVersion = $licenseInfo.Version
-        LatestVersion = $licenseInfo.Version
+        CurrentVersion = $currentVersion
+        LatestVersion = $latestVersion
         License = $licenseInfo.License
     }
 }
 
 foreach ($dep in $latestDependencies.Keys) {
-    if ($licensesParsed.ContainsKey($dep)) {
-        $licenseInfo = $licensesParsed[$dep]
-        $output += [PSCustomObject]@{
-            Dependency = $dep
-            CurrentVersion = $licenseInfo.Version
-            LatestVersion = $latestDependencies[$dep]
-            License = $licenseInfo.License
-        }
-    } else {
+    if (-not $licensesParsed.ContainsKey($dep)) {
         $output += [PSCustomObject]@{
             Dependency = $dep
             CurrentVersion = "N/A"
@@ -64,3 +131,4 @@ $output = $output | Where-Object {
 
 # Write output to file
 $output | Format-Table -AutoSize | Out-File $outputFile -Encoding UTF8
+
